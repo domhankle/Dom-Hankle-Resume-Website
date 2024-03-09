5791432d9a9b649f9719ee6f45a3d101f95627dd
Dom Hankle <domhankle03@gmail.com>
2024-03-08 20:25:09 -0500

    Working on header component
    Also added font awesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../components/navigation/header.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
